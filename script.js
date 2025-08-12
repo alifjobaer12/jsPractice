80a69c17a77fb8fcee5e8926842b1d81683c7995
alifjobaer12 <alifjobaer12@gmail.com>
2025-08-13 00:13:02 +0600

    13/08/25

---
+++ script.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector("button");
+let para = document.querySelector("p");
+console.log(btn);
+// if()
+// btn.innerText = "Hi jobaer"
+// btn.onclick == true;
+// if(btn.onclick === true) {
+    para.innerText = "Hi Jobaer";
+    para.classList.add("para2");
+// }
+
